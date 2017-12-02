e8fc09de68c9e6e629a50ccadcce56753235ac30
Claudia Maderthaner <cmaderthaner@gmail.com>
2017-12-02 21:13:47 +0100

    Fix course header.

--- src/assets/main.js
+++ src/assets/main.js
@@ -7,7 +7,8 @@ $(function() {
       for(var i = 0; i < response.courses.completed.length; i++) {
         var course = response.courses.completed[i];
         var container = $('<div></div>').addClass('course');
-        container.append('<h3></h3>').text(course.title);
+        var header = $('<h3></h3>').text(course.title);
+        container.append(header);
         var img = $('<img />').attr('src', course.badge);
         container.append(img);
         var link = $('<a></a>').attr('href', course.url)
