dac972d949b03a2c0c77a76d8a1ec92e682719a0
Claudia Maderthaner <cmaderthaner@gmail.com>
2017-12-02 21:11:33 +0100

    Show completed code school courses.

--- assets/main.js
+++ assets/main.js
@@ -1,5 +1,22 @@
 $(function() {
 
-  // your code will go here
+  $.ajax({
+    url: 'https://www.codeschool.com/users/cmaderthaner.json',
+    dataType: 'jsonp',
+    success: function(response) {
+      for(var i = 0; i < response.courses.completed.length; i++) {
+        var course = response.courses.completed[i];
+        var container = $('<div></div>').addClass('course');
+        container.append('<h3></h3>').text(course.title);
+        var img = $('<img />').attr('src', course.badge);
+        container.append(img);
+        var link = $('<a></a>').attr('href', course.url)
+          .attr('target', '_blank').addClass('btn btn-primary')
+          .text('See Course');
+        container.append(link);
+        $('#badges').append(container);
+      }
+    }
+  })
 
 });
